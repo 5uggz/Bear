2ded25c5644988ac561bcdfde713fe1662470c6e
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-24 22:09:48 -0400

    Create proxy.py

---
+++ proxy.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+def proxy_handler(client_socket, remote_host, remote_port):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+    
+    while True:
+        data = client_socket.recv(4096)
+        if len(data) == 0:
+            break
+        remote_socket.send(data)
+        
+        remote_data = remote_socket.recv(4096)
+        if len(remote_data) == 0:
+            break
+        client_socket.send(remote_data)
+    
+    client_socket.close()
+    remote_socket.close()
+
+def main():
+    # Set up the listening socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("0.0.0.0", 8888))
+    server.listen(5)
+    
+    print("Proxy server is listening on port 8888...")
+    
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Accepted connection from {addr[0]}:{addr[1]}")
+        
+        # Create a thread to handle the client
+        proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, "www.example.com", 80))
+        proxy_thread.start()
+
+if __name__ == '__main__':
+    main()
